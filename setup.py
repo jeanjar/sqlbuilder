33a1ae992e9aa0ff8cd365fa16cd7cdf04e2571b
Jean Jar Pereira de Araújo <jeanjar@iMac-de-Jean.local>
2015-07-20 13:05:17 -0300

    Adding setup.py file

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 setup(
     name='sqlbuilder',
     version='1.0',
-    py_modules=['sqlbuilder']
+    py_modules=['sqlbuilder'],
     url='https://github.com/jeanjar/sqlbuilder',
-    author='Jean Jar Pereira de Araújo',
+    author='Jean Jar Pereira de Araujo',
     author_email='jeanjpa@gmail.com',
     description='Fork from https://github.com/lanzz/sqlbuilder',
 )
