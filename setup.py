737f065b25b3adf0d63ea98cc1b0a4c2ba9fe033
Jean Jar Pereira de Araújo <jeanjpa@gmail.com>
2015-07-20 15:55:28 +0000

    pip setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='sqlbuilder',
+    version='1.0',
+    py_modules=['sqlbuilder']
+    url='https://github.com/jeanjar/sqlbuilder',
+    author='Jean Jar Pereira de Araújo',
+    author_email='jeanjpa@gmail.com',
+    description='Fork from https://github.com/lanzz/sqlbuilder',
+)
